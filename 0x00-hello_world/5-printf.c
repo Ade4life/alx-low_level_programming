67ccdb8c35e6474140560e7e6eb59c3725323022
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2022-12-10 10:55:33 -0800

    5-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *  main - entry point
+ * main - entry point
  *
- *  Return: Always O (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
 }
+
