a2b1de905e315efef10671ebeaab142ba4f3b988
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2022-12-10 08:11:17 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  * main - entry point
  *
  *  Return: Always O (Success)
- */
+*/
 
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (o);
+       puts("\"programming is like building a multilingual puzzle");
+       return (o);
 }
