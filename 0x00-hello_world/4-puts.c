9094fc01c5827ab82fb7c05d00f6118edfb99dc6
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2022-12-10 05:38:13 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *  Return: Always O (Success)
+ */
+
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (o);
+}
