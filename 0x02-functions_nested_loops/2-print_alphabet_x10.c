c56abd9efd92ee41e64eaf7f264fd467f9753060
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2022-12-14 12:32:49 -0800

    2-print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets ten time
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+		char count, lower;
+
+			for (count = '0'; count <= '9'; count++)
+			{
+				for (lower = 'a'; lower <= 'z'; lower++)
+				{
+					_putchar(lower);
+				}
+				_putchar('\n');
+			}
+}
+
+			
+
+														
+
+
