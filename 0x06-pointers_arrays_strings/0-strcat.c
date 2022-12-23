e21ae2dc9508f6f1cfda17fa9d2322da59bb0f34
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2022-12-22 21:50:20 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,11 @@ char *_strcat(char *dest, char *src)
 		int index = 0, dest_length = 0;
 
 			while (dest[index++])
-						dest_length++;
 
-	for (index = 0; src[index]; index++)
+			        dest_length++;
+
+		        for (index = 0; src[index]; index++)
+
 					dest[dest_length++] = src[index];
 
 					return (dest);
