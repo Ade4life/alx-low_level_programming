9d3226759a28d4316207773390dd5fa2ca381e31
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2023-01-12 01:11:16 -0800

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,19 @@
 
 void print_square(int size)
 {
-		int length, breadth;
+int length, breadth;
 
-			if (size > 0)
-					{
-				for (length = 0; length < size; length++)										{
-														for (breadth = 0; breadth < size; breadth++)																				_putchar('#')													if (length == size - 1
-		continue;																			_putchar('\n');
-																							}
-									}
+if (size > 0)
+{
+for (length = 0; length < size; length++)
+{
+for (breadth = 0; breadth < size; breadth++)
+_putchar('#')
+if (length == size - 1
+continue;
+_putchar('\n');
+}
+}
 
-				_putchar('\n');
+_putchar('\n');
 }
