505f6b3fe30e8f16fefbe90c98520dab987dce80
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2023-01-12 00:36:05 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,13 @@ int times, num;
 
 for (times = 0; times <= 9; times++)
 {
-for (num = 0; num <= 14; num++)											
+for (num = 0; num <= 14; num++)
 {
 if (num > 9)
 {
 _putchar((num / 10) + '0');
 }
-_putchar((num %10) + '0');
+_putchar((num % 10) + '0');
 }
 _putchar('\n');
 }
