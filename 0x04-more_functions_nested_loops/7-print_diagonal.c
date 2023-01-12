3d2426ad09bb382933314c1904eb7738101de341
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2023-01-12 00:56:16 -0800

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,19 +8,21 @@
 
 void print_diagonal(int n)
 {
-		int line, space;
+int line, space;
 
-			if (n > 0)
-					{
-						for (line = 0; line < n; line++)
-											{
-															for (space = 0; space < line; space++)
-																	_putchar(' ');
-																		_putchar('\\');																				if (line == n - 1)
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+{
+for (space = 0; space < line; space++)
+_putchar(' ');
+_putchar('\\');
 
-																		continue;
-																	_putchar('\n');																								}
+if (line == n - 1)
+continue;
+_putchar('\n');
+}
+}
 
-									}
-				_putchar('\n');
+_putchar('\n');
 }
