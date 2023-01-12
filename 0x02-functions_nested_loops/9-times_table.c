ff92ec70e3aa5edc9905fa09d4bae0bb3be9ab54
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2023-01-12 03:48:08 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,24 @@
 
 void times_table(void)
 {
-		int i, j, prod;
+int i, j, prod;
 
-			for (i = 0; i <= 9; i++)
-					{
-								_putchar('0');
+for (i = 0; i <= 9; i++)
+{
+_putchar('0');
 
-										for (j = 1; j <= 9; j++)
-													{
-																	_putchar(',');
-																				_putchar(' ');
+for (j = 1; j <= 9; j++)
+{
+_putchar(',');
+_putchar(' ');
+prod = i * j;
 
-																				prod = i * j;
-																										if (prod <= 9)
-																															_putchar(' ');
-																													else
-																																	_putchar((prod / 10) + '0');
-																																_putchar((prod % 10) + '0');
-																																		}
-												_putchar('\n');
-													}
+if (prod <= 9)
+_putchar(' ');
+else
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
 }
