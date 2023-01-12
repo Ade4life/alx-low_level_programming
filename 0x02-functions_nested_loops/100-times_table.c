8505e272350a33a6f947dd6fb6538dc62518cea5
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2023-01-12 04:19:30 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,66 +7,39 @@
 
 void print_times_table(int n)
 {
-		int i, j, prod;
+int i, j, prod;
 
-			if (n >= 0 && n <= 15)
-					{
-								for (i = 0; i <= n; i++)
-											{
-															_putchar('0');
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar('0')
 
+for (j = 1; j <= n; j++)
+{
+_putchar(',');
+_putchar(' ');
 
+prod = i * j;
 
-																		for (j = 1; j <= n; j++)
+if (prod <= 99)
+_putchar(' ');
 
-																						{
+if (prod <= 9)
+_putchar(' ');
 
-																											_putchar(',');
-
-																															_putchar(' ');
-
-
-
-																																			prod = i * j;
-
-
-
-																																							if (prod <= 99)
-
-																																													_putchar(' ');
-
-																																											if (prod <= 9)
-
-																																																	_putchar(' ');
-
-
-
-																																															if (prod >= 100)
-
-																																																				{
-
-																																																										_putchar((prod / 100) + '0');
-
-																																																															_putchar(((prod / 10)) % 10 + '0');
-
-																																																																			}
-
-																																																			else if (prod <= 99 && prod >= 10)
-
-																																																								{
-
-																																																														_putchar((prod / 10) + '0');
-
-																																																																		}
-
-																																																							_putchar((prod % 10) + '0');
-
-																																																										}
-
-																					_putchar('\n');
-
-																							}
-
-									}
-
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
