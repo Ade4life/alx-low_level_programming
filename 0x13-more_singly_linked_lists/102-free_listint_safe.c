3ad248468fe7ca484ce1121332a611380966e0d8
Engr Adebayo-Qasim Taofiq <aladeadebayo55@gmail.com>
2023-01-31 11:00:15 -0800

    set it free

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,8 +4,8 @@
  * free_listp2 - frees a linked list
  * @head: head of a list.
  *
- *     * Return: no return.
- *      */
+ * Return: no return.
+ */
 void free_listp2(listp_t **head)
 {
 listp_t *temp;
